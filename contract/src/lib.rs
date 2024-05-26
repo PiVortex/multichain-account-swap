d90940a77d6814683f0078a4342ab3ad7690857c
PiVortex <owenhassall@hotmail.co.uk>
2024-05-26 18:24:13 +0100

    added create_trade

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,5 +1,5 @@
-use near_sdk::{near, AccountId};
-use near_sdk::collections::{UnorderedMap};
+use near_sdk::{near, env, AccountId};
+use near_sdk::collections::UnorderedMap;
 
 #[near(contract_state)]
 pub struct Contract {
@@ -12,11 +12,11 @@ pub struct Trade {
     seller_id: AccountId,
     buyer_id: Option<AccountId>,
     eth_add: String,
-    btc_add: String,
+    btc_add: Option<String>,
     nft_contract: String,
     nft_id: u32,
     sale_price: u32,
-    swapped: Bool
+    swapped: bool
 }
 
 impl Default for Contract {
@@ -31,31 +31,51 @@ impl Default for Contract {
 #[near]
 impl Contract {
 
-    pub fn create_trade(&mut self, nft_contract: String, sale_price: u32) -> u32 {
+    pub fn create_trade(&mut self, eth_add: String, nft_contract: String, nft_id: u32, sale_price: u32) -> String {
+
+        let seller_id = env::predecessor_account_id();
+
+        self.trade_count += 1;
+        let trade_id = self.trade_count.to_string();
+
+        let new_trade = Trade {
+            seller_id,
+            buyer_id: None,
+            eth_add,
+            btc_add: None,
+            nft_contract,
+            nft_id,
+            sale_price,
+            swapped: false
+        };
+
+        self.trades.insert(&trade_id, &new_trade);
+
+        trade_id
 
     }
 
-    pub fn accept_trade(&mut self) {
+    // pub fn accept_trade(&mut self) {
 
-    }
+    // }
 
-    pub fn confirm_trade(&mut self) {
+    // pub fn confirm_trade(&mut self) {
 
-    }
+    // }
 
-    pub fn withdraw_nft(&mut self, payload: String) {
+    // pub fn withdraw_nft(&mut self, payload: String) {
 
-    }
+    // }
 
-    pub fn withdraw_btc(&mut self, paylod: String) {
+    // pub fn withdraw_btc(&mut self, paylod: String) {
 
-    }
+    // }
     
-    pub fn get_all_trades(&self) -> Vec<String, Trade> {
+    // pub fn get_all_trades(&self) -> Vec<String, Trade> {
 
-    }
+    // }
 
-    pub fn get_trade(&self) -> Trade {
+    // pub fn get_trade(&self) -> Trade {
 
-    }
+    // }
 }
